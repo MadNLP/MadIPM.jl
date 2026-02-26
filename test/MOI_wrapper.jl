eee42ffd2c0c1c8fe73e62bd28e6a30b4ce6599e
Alexis Montoison <alexis.montoison@polymtl.ca>
2026-02-25 21:56:00 -0600

    Fix the MOI tests

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -8,13 +8,14 @@ import MadIPM
 function test_runtests()
     excludes = [
         "test_model_copy_to_UnsupportedAttribute",
-        # Fixed variables: require MadNLP 0.9 for correct dual multipliers
-        "test_solve_VariableIndex_ConstraintDual_MAX_SENSE",
         r"^test_linear_integration$",
         # Currently not supported
         "test_linear_complex",
         "test_quadratic",
         "test_conic",
+        # MadNLP.SymbolicException() with MUMPS
+        "test_solve_VariableIndex_ConstraintDual_MIN_SENSE",
+        "test_solve_VariableIndex_ConstraintDual_MAX_SENSE",
     ]
     model = MOI.instantiate(MadIPM.Optimizer, with_bridge_type = Float64)
     MOI.set(model, MOI.Silent(), true) # comment this to enable output
