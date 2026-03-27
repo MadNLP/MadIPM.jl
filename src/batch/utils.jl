acc0e2e4fba55bc933b8059c4a8868e94beb2cfb
Klamkin, Michael <mklamkin3@gatech.edu>
2026-03-27 17:50:13 -0400

    use BQM's mapreduce for max, min, sum

--- src/batch/utils.jl
+++ src/batch/utils.jl
@@ -8,9 +8,12 @@ function _csc_with_nzval(A::SparseArrays.SparseMatrixCSC, nzval, n)
     return SparseArrays.SparseMatrixCSC(n, n, SparseArrays.getcolptr(A), SparseArrays.rowvals(A), nzval)
 end
 
-batch_maximum!(out::AbstractMatrix, src::AbstractMatrix) = maximum!(out, src)
-batch_minimum!(out::AbstractMatrix, src::AbstractMatrix) = minimum!(out, src)
-batch_sum!(out::AbstractMatrix, src::AbstractMatrix) = sum!(out, src)
+batch_maximum!(out::AbstractMatrix{T}, src::AbstractMatrix{T}) where {T} =
+    batch_mapreduce!(identity, max, typemin(T), out, src)
+batch_minimum!(out::AbstractMatrix{T}, src::AbstractMatrix{T}) where {T} =
+    batch_mapreduce!(identity, min, typemax(T), out, src)
+batch_sum!(out::AbstractMatrix{T}, src::AbstractMatrix{T}) where {T} =
+    batch_mapreduce!(identity, +, zero(T), out, src)
 
 function zero_inactive_step!(batch_solver::AbstractBatchMPCSolver{T}) where T
     ws = batch_solver.workspace
